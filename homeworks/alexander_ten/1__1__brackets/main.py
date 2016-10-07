5f2edd6c0166b9b06b99c8e38ceda84af78b44cc
Alexander Ten <orionapplepie@gmail.com>
2016-10-08 09:35:26 +1000

    Coomit

--- homeworks/alexander_ten/1__1__brackets/main.py
+++ homeworks/alexander_ten/1__1__brackets/main.py
@@ -1,3 +1,27 @@
-i = 0
-print(i)
-a = 1
+import re
+import sys
+
+
+def brackets_check(s):
+    meetings = 0
+    br = ['[', ']', '(', ')', '{', '}']
+    for c in s:
+        if c == '(':
+            meetings += 1
+        elif c == ')':
+            meetings -= 1
+            if meetings < 0:
+                return False
+
+    return meetings == 0
+
+
+str1 = "( cool qwerty 123 (ttt[ ) ] ( )rrr [qwerty {!!!@#$!@#$@! (*) &^%^&*!&%#} ] )) [][]  ([{ )"
+pattern = re.findall(r'\[\d+\]', str1)
+print(pattern)
+
+pattern2 = re.sub(r'\([^)]*$', "", str1)
+print(pattern2)
+
+res = brackets_check(str1)
+print(res)
