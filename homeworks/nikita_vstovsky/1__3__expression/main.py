c0c6fb6e75a3493f71007949fa542163d3a0be29
Nikita Vstovsky <nikvst@gmail.com>
2016-10-08 18:28:09 +1100

    Задание 1.3

---
+++ homeworks/nikita_vstovsky/1__3__expression/main.py
@@ -0,0 +1,20 @@
+import re
+
+
+def calculate(my_string):
+    eval(my_string)
+
+
+def find_expression(my_string):
+    result = re.findall(r'-?[ ]?([0-9]+\.?[0-9]*)[ ]?([\+\-\*/])[ ]?([0-9]+\.?[0-9]*)', my_string)
+    return result
+
+
+def calc_expression(my_string):
+    for expr in find_expression(my_string):
+        expr_str = ''.join(map(str, expr))
+        result = eval(expr_str)
+        print("%s = %s" % (expr_str, result))
+
+new_string = input("Введите строку: ")
+calc_expression(new_string)
